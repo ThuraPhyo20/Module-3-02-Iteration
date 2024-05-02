510c8cb993a8a13651747334065ed0fab2c213d4
ThuRaPhyo <thuyaphyo7877@gmail.com>
2024-05-02 13:39:21 +0630

    solved

---
+++ script.js
@@ -0,0 +1,8 @@
+// TODO: Write Your JavaScript Code Here
+
+var students = ["Hsu Hsu", "Wady", "Sein", "Shwe", "Hnin"];
+console.log(students.length);
+
+for (var i = 0; i < students.length; i++){
+    console.log(students[i]);
+};
